29c677a10faa9608b73293bd844d575f7c9dc35c
Ngo Van Thang <thangnvbkhn@gmail.com>
2016-01-07 00:23:53 +0900

    Completed chap2

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -3,11 +3,26 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>Travel</title>
+    <title><?php wp_title(' | ', true, 'right'); ?><?php bloginfo('name'); ?></title>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <link href='https://fonts.googleapis.com/css?family=Acme' rel='stylesheet' type='text/css'>
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+    <header>
+        <div class="siteinfo">
+            <div class="container">
+                <h1>
+                    <a href="<?php home_url(); ?>"><?php bloginfo('name'); ?></a>
+                </h1>
+                <p><?php bloginfo('description'); ?></p>
+            </div>
+        </div>
+    </header>
+
     <div class="container">
     <?php if (have_posts()): ?>
         <?php while(have_posts()): ?>
@@ -35,11 +50,11 @@
                 <?php if (is_single()): ?>
                 <div class="pagenav">
                     <span class="old">
-                        <?php previous_post_link('%link','<i class="fa fa-chevron-circle-left"></i> %title'); ?>
+                        <?php next_post_link('%link', '<i class="fa fa-chevron-circle-right"></i> 前ページへ'); ?>
                     </span>
 
                     <span class="new">
-                        <?php next_post_link('%link', '%title <i class="fa fa-chevron-circle-right"></i>'); ?>
+                        <?php previous_post_link('%link','次ページへ <i class="fa fa-chevron-circle-left"></i>'); ?>
                     </span>
                 </div>
                 <?php endif; ?>
@@ -59,5 +74,13 @@
         <?php endwhile; ?>
     <?php endif; ?>
     </div> <!-- container -->
+
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; <?php bloginfo('name'); ?></small>
+        </div>
+    </footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
